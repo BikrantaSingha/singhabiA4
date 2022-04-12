8d9cd634f499b1d56c4aaea3803915a9a907c5a7
BikrantaSingha <singhabi@sheridancollege.ca>
2022-04-12 08:11:34 -0400

    planets interface setup complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Details } from './details';
 import { DetailsService } from './details.service';
+import { Planets } from './planets';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,11 @@ import { DetailsService } from './details.service';
 export class AppComponent {
   title = 'singhabiA4';
   myDetails: Details;
+  myPlanets: Planets;
 
   constructor(private _detailsService: DetailsService){
     this.myDetails = this._detailsService.getDetails().myDetails;
+    this.myPlanets = this._detailsService.getDetails().solarSystem;
   }
 
   ngOnInit() {
---
+++ src/app/planets.ts
@@ -0,0 +1,9 @@
+export interface Planets{
+	/*planetName: string;
+	planetColor: string;
+	planetRadiusKM: number;
+	distInMillionsKM: Object;
+	image: string;
+	*/
+	planets: Object[];
+}
